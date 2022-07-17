00195d7fabcc7c81517f191b5ffd41f6331c5e47
Franco <franciscojavierelpro@gmail.com>
2022-07-16 19:54:10 -0500

    first commit

---
+++ Leccion01/01-01-HolaMundo.js
@@ -0,0 +1,41 @@
+//Dato String
+var nombre = "Jse";
+console.log(nombre);
+
+//Dato numerico
+var numero = 1000;
+console.log(numero)
+
+//Dato objeto
+
+var objeto = {
+    nombre : "Fran",
+    apellido : "Serrano",
+    edad : 18
+}
+
+//TIpo de dato funcion
+function name(nombre){
+    console.log(nombre);
+}
+
+//Tipo de dato simbolo
+var simbolo = Symbol("Mi tipo de dato");
+
+//Tipo da dato clase tambien es un tipo de dato funcion
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+//Tipo de dato undefined
+var x;
+
+//Tipo de dato null = ausencia de valor
+var y = null;
+
+
+var autos = ["BMW","AUDI","VOLVO"];
+console.log(autos)
---
+++ Leccion01/01-02-Concatenar.js
@@ -0,0 +1,7 @@
+var nombre = "Fran";
+
+var apellido = " Serrano";
+
+var nombreCompleto = nombre + apellido;
+
+console.log(nombreCompleto);
